959a8480af9fe28423aeb6cfcb57c4e363fa1912
Christian Silfang (comcy) <christian.silfang@gmail.com>
2025-10-23 22:29:42 +0200

    Added pipe samples

--- fitness-tracker-web/projects/fitness-tracker-app/src/app/exercises/exercises.ts
+++ fitness-tracker-web/projects/fitness-tracker-app/src/app/exercises/exercises.ts
@@ -1,16 +1,19 @@
 import { Component, inject } from '@angular/core';
 import { ExerciseLogicService } from '../../../../exercise-lib/src/lib/logic-services/exercise-logic.service'
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-exercises',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './exercises.html',
   styleUrl: './exercises.scss'
 })
 export class Exercises {
 
-  public exerciseService = inject(ExerciseLogicService);
 
+  public exerciseService = inject(ExerciseLogicService);
+  public name: string = 'name';
+  public dateofToday: Date = new Date();
 
 }
