c2f7a33dd6da278a5477a63400a6f93fbe6a0820
Christian Silfang (comcy) <christian.silfang@gmail.com>
2025-10-30 20:33:13 +0100

    Added angular material and update basic style.

--- fitness-tracker-web/projects/fitness-tracker-app/src/app/app.ts
+++ fitness-tracker-web/projects/fitness-tracker-app/src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterModule],
+  imports: [RouterOutlet, RouterModule, MatToolbarModule, MatButtonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
