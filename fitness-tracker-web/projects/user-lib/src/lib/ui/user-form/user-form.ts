f59bc45c4da4a454427f6c1a40a263e30632f80e
Christian Silfang (comcy) <christian.silfang@gmail.com>
2025-10-31 12:27:17 +0100

    Updated form, profile, etc ...

--- fitness-tracker-web/projects/user-lib/src/lib/ui/user-form/user-form.ts
+++ fitness-tracker-web/projects/user-lib/src/lib/ui/user-form/user-form.ts
@@ -4,10 +4,11 @@ import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @Component({
   selector: 'lib-user-form',
-  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule],
+  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatDialogModule],
   templateUrl: './user-form.html',
   styleUrl: './user-form.scss',
   standalone: true
@@ -16,10 +17,11 @@ export class UserFormComponent implements OnChanges {
   private fb = inject(FormBuilder);
 
   @Input() user: User | undefined;
-  @Input() buttonVisible: boolean | undefined = true;
   @Input() buttonText: string | undefined = 'Save';
-
+  @Input() titleText: string = 'Add new user';
+  @Input() buttonVisible: boolean | undefined = true;
   @Output() save = new EventEmitter<User>();
+  @Output() cancel = new EventEmitter<void>();
 
   form = this.fb.group({
     name: ['', Validators.required],
@@ -39,5 +41,9 @@ export class UserFormComponent implements OnChanges {
     if (this.form.valid) {
       this.save.emit(this.form.value as User);
     }
+  }
+
+  onCancel() {
+    this.cancel.emit();
   }
 }
--- fitness-tracker-web/projects/user-lib/src/lib/view/add-user-dialog/add-user-dialog.ts
+++ fitness-tracker-web/projects/user-lib/src/lib/view/add-user-dialog/add-user-dialog.ts
@@ -1,29 +1,40 @@
 import { Component, inject } from '@angular/core';
-import { Router, RouterModule } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+import { Router, ActivatedRoute } from '@angular/router';
 import { UserFormComponent } from '../../ui/user-form/user-form';
 import { UserBusinessService } from '../../services/business/user-business.service';
 import { User } from '../../domain/models/user';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'lib-add-user-dialog',
   standalone: true,
-  imports: [RouterModule, UserFormComponent, MatDialogModule, MatButtonModule],
-  templateUrl: './add-user-dialog.html',
-  styleUrl: './add-user-dialog.scss',
+  imports: [],
+  template: '',
 })
 export class AddUserDialogComponent {
-  private userBusinessService = inject(UserBusinessService);
+  private dialog = inject(MatDialog);
   private router = inject(Router);
+  private route = inject(ActivatedRoute);
+  private userBusinessService = inject(UserBusinessService);
 
-  onSave(user: User) {
-    this.userBusinessService.addUser(user).subscribe(() => {
-      this.onClose();
+  constructor() {
+    const dialogRef = this.dialog.open(UserFormComponent, {
+      width: '400px',
+      disableClose: true,
     });
-  }
 
-  onClose() {
-    this.router.navigate(['.', { outlets: { dialog: null } }]);
+    dialogRef.componentInstance.save.subscribe((user: User) => {
+      this.userBusinessService.addUser(user).subscribe(() => {
+        dialogRef.close();
+      });
+    });
+
+    dialogRef.componentInstance.cancel.subscribe(() => {
+      dialogRef.close();
+    });
+
+    dialogRef.afterClosed().subscribe(() => {
+      this.router.navigate(['.', { outlets: { dialog: null } }], { relativeTo: this.route.parent });
+    });
   }
 }
